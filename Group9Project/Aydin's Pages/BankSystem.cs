822f3d038d69480d9b5f7aa5c8e496f7a98b11dd
Aydin Battal <battalay@sheridancollege.ca>
2020-04-19 16:21:26 -0400

    Implemented properties and methods needed for BankSystem class. This class is responsible for balance and overdraft calculations.

--- Group9Project/Aydin's Pages/BankSystem.cs
+++ Group9Project/Aydin's Pages/BankSystem.cs
@@ -8,5 +8,41 @@ namespace Group9Project.Aydin_s_Pages
 {
     class BankSystem
     {
+        public double Balance { get; protected set; }
+        public double OverdraftAmount { get; set; }
+        public string User { get; set; }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public BankSystem()
+        {
+            Balance = 100;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > Balance)
+                throw new InvalidOperationException("Not enough money");
+
+            if (Balance + OverdraftAmount < 0)
+                throw new Exception();
+
+
+            Balance -= amount;
+
+            if (Balance <= 0)
+            {
+                Balance -= 100;
+            }
+
+        }
+
+        public void UpdateBalance(double balance)
+        {
+            Balance = balance;
+        }
     }
 }
