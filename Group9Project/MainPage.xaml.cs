89b1c3863403ca327e56e37a8286ad94e6746a37
Karan <pate1228@sheridancollege.com>
2020-04-18 21:22:11 -0400

    Temp mainpage commit to test airplane

--- Group9Project/MainPage.xaml.cs
+++ Group9Project/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using AppDevFinalProject.Karan_s_Pages;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,11 @@ namespace Group9Project
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void AirplaneButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Airplane));
         }
     }
 }
