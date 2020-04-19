36bdcfc5e8a747998ff3a7a055e4edcace57d012
Aydin Battal <battalay@sheridancollege.ca>
2020-04-19 16:19:45 -0400

    Implemented the code behind BankPage.

--- Group9Project/Aydin's Pages/BankPage.xaml.cs
+++ Group9Project/Aydin's Pages/BankPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,57 @@ namespace Group9Project.Aydin_s_Pages
     /// </summary>
     public sealed partial class BankPage : Page
     {
+        BankSystem bank = new BankSystem();
+
+        private InvoiceSystem _invoice = new InvoiceSystem();
+
         public BankPage()
         {
             this.InitializeComponent();
+            PlaceholderDate.Text = DateTime.Now.ToString();
+            PlaceholderBalance.Text = bank.Balance.ToString();
+            PlaceholderOverdraft.Text = bank.OverdraftAmount.ToString();
+        }
+
+        private void AppBarButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //todo: implement going back to mainsite
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            List<InvoiceSystem> invoices = e.Parameter as List<InvoiceSystem>;
+            if (invoices != null)
+            {
+                foreach (var invoice in invoices)
+                {
+                    _invoice.InvoiceList.Add(invoice);
+                    bank.UpdateBalance(invoice.TempBalance);
+                }
+            }
+            PlaceholderBalance.Text = bank.Balance.ToString();
+
+        }
+        private void ProcessButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            double depositAmt = double.Parse(DepositAmt.Text);
+            double withdrawAmt = double.Parse(WithdrawAmt.Text);
+
+            bank.Deposit(depositAmt);
+            bank.Withdraw(withdrawAmt);
+            _invoice.AddInvoice(depositAmt, withdrawAmt, bank.Balance);
+
+            int count = _invoice.InvoiceList.Count;
+            MessageDialog dialog = new MessageDialog($"Invoice Number = {_invoice.InvoiceList[count - 1].InvoiceNumber} \n Date Processed = {_invoice.InvoiceList[count - 1].Date} \n Deposit Amount = {DepositAmt.Text} \n Withdraw Amount = {WithdrawAmt.Text}");
+            dialog.ShowAsync();
+
+            PlaceholderBalance.Text = bank.Balance.ToString();
+        }
+
+
+        private void InvoiceHistoryButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(InvoiceHistoryPage), _invoice.InvoiceList);
         }
     }
 }
